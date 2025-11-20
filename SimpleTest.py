d9cc6f82054be3910e78ed5a471831dd73bf882b
gmoysan <gwenael.moysan@ansys.com>
2025-11-20 15:46:26 +0100

    changed tkinker colour from blue to red

--- SimpleTest.py
+++ SimpleTest.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 root = tk.Tk()
 root.title("Tk Example")
-root.configure(background="darkblue")
+root.configure(background="red")
 root.minsize(200, 200)
 root.maxsize(500, 500)
 root.geometry("300x300+50+50")
